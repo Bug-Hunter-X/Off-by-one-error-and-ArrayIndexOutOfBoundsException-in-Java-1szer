fc543d1706845b62e398c38de3ddc06fec3b2286
BugHunter-X <askzenai@gmail.com>
2024-12-14 22:22:06 +0100

    Add bugSolution.java file

---
+++ bugSolution.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) { // Corrected loop condition
+            arr[i] = i * 2; 
+        }
+        //System.out.println(arr[5]); // Removed out of bounds access
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
